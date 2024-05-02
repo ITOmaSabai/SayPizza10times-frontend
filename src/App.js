f51922a84bdcaaabb29b5506a74c9941219373b4
ItoSbai <lualaa.design2565@gmail.com>
2024-05-02 12:35:58 +0900

    add:クイズと結果を表示するGameLayoutコンポーネントを作成

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import logo from './logo.svg';
 // import './App.css';
 import { HeroLayout } from './components/layouts/HeroLayout';
+import QuizGame from './components/layouts/GameLayout';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <HeroLayout />
+        <QuizGame />
       </header>
     </div>
   );
